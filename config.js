d02006faad9524011497fc3f7037b2c0177e25f5
DominicDev <s0l0estudi4r@gmail.com>
2026-04-03 20:23:33 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'SNOOP DOGG';
+export const nameSongs = [
+	"SKY'S THE LIMIT", /*1*/
+	"DROP IT LIKE IT'S HOT ", /*2*/
+	"SWEAT", /*3*/
+	"BEAUTIFUL", /*4*/
+	"SENSUAL SEDUCTION", /*5*/
+	"VATO", /*6*/
+	"FRENCH_INHALE", /*7*/
+	"GIN AND JUICE", /*8*/
+	"SMOKE THE WEED", /*9*/
+	"WHO AM I", /*10*/
+	"THAT GOOD", /*11*/
+	"BOSS' LIFE", /*12*/
+	"BOOM", /*13*/
+	"BIG SUBWOOFER", /*14*/
+	"LET'S GET BLOWN", /*15*/
+	"BITCH PLEASE", /*16*/
+	"SNOOP DOGG", /*17*/
+	"UPS & DOWNS/BANG OUT", /*18*/
+	"TIRED OF RUNNING", /*19*/
+	"WET", /*20*/
+]
